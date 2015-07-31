e49f99ee5fde8a7f423369b310f266430b08238b
James Robson <james.robson@pkware.com>
2015-07-31 12:05:56 -0600

    added missing descriptor + error.gsp

---
+++ RatelimitGrailsPlugin.groovy
@@ -0,0 +1,17 @@
+class RatelimitGrailsPlugin {
+    def version = "0.1"
+    def grailsVersion = "2.5 > *"
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+    def title = "Ratelimit Plugin" // Headline display name of the plugin
+    def author = "James R."
+    def authorEmail = "traptoy@gmail.com"
+    def description = '''\
+Simple value-based rate limiting plugin. Not intended to replace actual server
+rate limiting, this plugin gives you a low calorie way of ensuring some piece of
+code, such as a controller action, can be limited by some an piece of data,
+such as IP address.
+
+'''
+}
